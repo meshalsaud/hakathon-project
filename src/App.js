c167e72fac8c2f6455e788f5c33b4c554116b07b
meshal-saud <meshal.aneze@gmail.com>
2018-08-01 22:49:52 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import scriptLoader from 'react-async-script-loader'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='container'>
+          <div className='main-page' 
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
